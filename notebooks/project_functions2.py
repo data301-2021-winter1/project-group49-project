7186511b0f96b58d1dc674fba3dad1dd7bc0d98c
ved-b <vballary@student.ubc.ca>
2021-11-01 17:20:05 -0700

    Milestone 3 Task 3 completed by Ved

---
+++ notebooks/project_functions2.py
@@ -0,0 +1,23 @@
+#Method chaining
+
+import pandas as pd
+
+def load_and_process(location):
+    
+
+# Method chaining begins
+
+    df = (   
+        pd.read_csv(location, sep = ",")
+        .drop([' 77516',' 13',' 2174',' 0', ' Not-in-family', ' 40'],axis=1)
+        .rename(columns={'39': 'Age', ' State-gov': 'Industry', ' Bachelors': 'Education'})
+        .rename(columns={' Never-married': 'Marital Stat', ' Adm-clerical': 'Position'})
+        .rename(columns={' White': 'Race', ' Male': 'Gender', ' United-States': 'Country', ' <=50K':'Income'})
+        .drop_duplicates()
+        .sort_values("Age", ascending=True)
+
+    )
+
+    return df
+
+
